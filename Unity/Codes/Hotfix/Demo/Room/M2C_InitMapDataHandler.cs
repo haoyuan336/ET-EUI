a0218411f5005fc237c68fcd20104d900e733232
chuhaoyuan <328387458@qq.com>
2022-03-08 14:18:42 +0800

    回滚代码

--- Unity/Codes/Hotfix/Demo/Room/M2C_InitMapDataHandler.cs
+++ Unity/Codes/Hotfix/Demo/Room/M2C_InitMapDataHandler.cs
@@ -4,7 +4,6 @@
     {
         protected override async ETTask Run(Session session, M2C_InitMapData message)
         {
-            
             Log.Debug("init map data" + message.DiamondInfo);
             await ETTask.CompletedTask;
         }
