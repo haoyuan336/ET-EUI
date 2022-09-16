a82f3341c65c84e02c42f50e0757dfc3f0dd8e33
haoyuan336 <328387458@qq.com>
2022-09-16 12:15:19 +0800

    1.增加新英雄

--- Unity/Codes/Model/Demo/HeroBagType.cs
+++ Unity/Codes/Model/Demo/HeroBagType.cs
@@ -1,22 +1,4 @@
 ﻿namespace ET
 {
-    public enum HeroBagType
-    {
-        Invalide = -1,
-        Hero = 1,
-        Materail = 2,
-        HeroAndMaterial = 3,
-    }
-
-    public enum HeroElementType
-    {
-        Invalide = -1,
-
-        // ElementName
-        Fire = 1,
-        Dark = 2,
-        Water = 3,
-        Wind = 4,
-        Light = 5
-    }
+  
 }
--- Unity/Codes/Model/Demo/TypeConfig.cs
+++ Unity/Codes/Model/Demo/TypeConfig.cs
@@ -176,4 +176,24 @@
         Damage = 1, //是否造成额外伤害
         Not = 0, //无额外伤害
     }
+
+    public enum HeroBagType
+    {
+        Invalide = -1,
+        Hero = 1,
+        Materail = 2,
+        HeroAndMaterial = 3,
+    }
+
+    public enum HeroElementType
+    {
+        Invalide = -1,
+
+        // ElementName
+        Fire = 1,
+        Dark = 2,
+        Water = 3,
+        Wind = 4,
+        Light = 5
+    }
 }
